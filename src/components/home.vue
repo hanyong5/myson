ecbb6439e7b5618f6ff2f5bb45ec809bea9c213a
hanyong5 <hanyong5@naver.com>
2021-09-13 22:20:59 +0900

    v6

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,5 +1,7 @@
 <template>
-  <p>준비중입니다.</p>
+  <div class="linkView">
+    <a href="http://m.cgntv.net/tv/31000/1.cgn" target="_blank">온라인 생명의 삶</a>
+  </div>
 </template>
 
 <script>
@@ -9,5 +11,5 @@ export default {
 </script>
 
 <style>
-
+.linkView a{display: block;line-height:50px;background:skyblue;text-align: center;font-weight: bold;font-size: 1.3em;border-radius:5px;color:white}
 </style>
