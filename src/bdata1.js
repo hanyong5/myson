f6c91b745f7753f52d7f603e6584c322c60262e5
hanyong5 <hanyong5@naver.com>
2021-09-17 11:36:36 +0900

    Update bdata1.js

--- src/bdata1.js
+++ src/bdata1.js
@@ -2,31 +2,31 @@ export default [
     {
         idx:0,list:'1과' ,title:'나의 신앙고백의 간증',
         line1:'로마서 10:9~10',line2:'마태복음 16:16',
-        con1:'<span>네</span>가 만일 <span>네 입</span>으로 <span>예수</span>를 주로 <span>시인</span>하며 또 <span>하나님</span>께서 그를 <span>죽은 자</span> 가운데서 <span>살리신 것</span>을 네 <span>마음</span>에 <span>믿</span>으면 <span>구원</span>을 얻으리니 <span>사람</span>이 <span>마음</span>으로 <span>믿</span>어 <span>의</span>에 이르고 <span>입</span>으로 <span>시인</span>하여 <span>구원</span>에 이르느니라',
-        con2:'<span>시몬 베드로</span>가 대답하여 가로되 <span>주</span>는 <span>그리스도</span>시요 살아계신 <span>하나님</span>의 <span>아들</span>이시니이다'
+        con1:'<span>네</span>가 만일 <span>네 입</span>으로 <span>예수</span>를 주로 <span>시인</span>하며 또 <span>하나님</span>께서 그를 <span>죽은 자</span> 가운데서 <span>살리신 것</span>을 네 <span>마음</span>에 <span>믿</span>으면 <span>구원</span>을 받으리라 <span>사람</span>이 <span>마음</span>으로 <span>믿</span>어 <span>의</span>에 이르고 <span>입</span>으로 <span>시인</span>하여 <span>구원</span>에 이르느니라',
+        con2:'<span>시몬 베드로</span>가 대답하여 이르되 <span>주</span>는 <span>그리스도</span>시요 살아계신 <span>하나님</span>의 <span>아들</span>이시니이다'
     },
     {
         idx:1,list:'2과',title:'하나님과 매일 만나는 생활',
         line1:'히브리서 4:16',line2:'예레미야애가 3:22~23',
         con1:'그러므로 <span>우리</span>가 <span>긍휼하심</span>을 받고 때를 따라 돕는 <span>은혜</span>를 얻기 위하여 <span>은혜</span>의 보좌 앞에 <span>담대히 나아갈 것</span>이니라',
-        con2:'<span>여호와</span>의 <span>자비</span>와 <span>긍휼</span>이 무궁하시므로 <span>우리</span>가 진멸되지 아니함이니이다. 이것이 <span>아침</span>마다 <span>새로우</span>니 <span>주의 성실</span>이 <span>크도소</span>이다'
+        con2:'<span>여호와</span>의 <span>인자</span>와 <span>긍휼</span>이 무궁하시므로 <span>우리</span>가 진멸되지 아니함이니이다. 이것들이 <span>아침</span>마다 <span>새로우</span>니 <span>주의 성실</span>이 <span>크시도소</span>이다'
     },
     {
         idx:2,list:'3과',title:'경건의 시간',
         line1:'시편 1:1~2',line2:'시편 119:105',
-        con1:'<span>복 있는 사람</span>은 <span>악인</span>의 꾀를 좇지 아니하며 <span>죄인</span>의 길에 서지 아니하며 <span>오만한 자</span>의 <span>자리</span>에 앉지 아니하고, <span>오직 여호와</span>의 <span>율법</span>을 <span>즐거워</span>하여 그 율법을 <span>주야</span>로 <span>묵상</span>하는 자로다',
+        con1:'<span>복 있는 사람</span>은 <span>악인</span>의 꾀를 따르지 아니하며 <span>죄인</span>들의 길에 서지 아니하며 <span>오만한 자</span>들의 <span>자리</span>에 앉지 아니하고, <span>오직 여호와</span>의 <span>율법</span>을 <span>즐거워</span>하여 그 율법을 <span>주야</span>로 <span>묵상</span>하는 자로다',
         con2:'<span>주의 말씀</span>은 내 <span>발</span>에 <span>등</span>이요 내 <span>길</span>에 <span>빛</span>이니이다'
     },
     {
         idx:3,list:'4과',title:'살았고 운동력 있는 말씀',
         line1:'로마서 1:16',line2:'디모데후서 3:16',
-        con1:'<span>내</span>가 <span>복음</span>을 부끄러워하지 아니하노니 이 <span>복음</span>은 모든 <span>믿는 자</span>에게 <span>구원</span>을 주시는 <span>하나님</span>의 <span>능력</span>이 됨이라 첫째는 <span>유대인</span>에게요 또한 <span>헬라인</span>에게로다',
+        con1:'<span>내</span>가 <span>복음</span>을 부끄러워하지 아니하노니 이 <span>복음</span>은 모든 <span>믿는 자</span>에게 <span>구원</span>을 주시는 <span>하나님</span>의 <span>능력</span>이 됨이라 먼저는 <span>유대인</span>에게요 또한 <span>헬라인</span>에게로다',
         con2:'<span>모든 성경</span>은 <span>하나님</span>의 감동으로 된 것으로 <span>교훈</span>과 <span>책망</span>과 <span>바르게 함</span>과 <span>의</span>로 <span>교육하기</span>에 유익하니'
     },
     {
         idx:4,list:'5과',title:'무엇이 바른 기도인가',
         line1:'빌립보서 4:6~7',line2:'마태복음 6:6',
-        con1:'<span>아무 것도 염려</span>하지 말고 <span>오직 모든 일</span>에 <span>기도</span>와 <span>간구</span>로, 너희 구할 것을 <span>감사함</span>으로 <span>하나님</span>께 아뢰라. 그리하면 <span>모든 지각</span>에 뛰어난 <span>하나님</span>의 <span>평강</span>이 그<span>리스도 예수 안</span>에서 <span>너희 마음</span>과 <span>생각</span>을 지키시리라',
+        con1:'<span>아무 것도 염려</span>하지 말고 <span>다만 모든 일</span>에 <span>기도</span>와 <span>간구</span>로, 너희 구할 것을 <span>감사함</span>으로 <span>하나님</span>께 아뢰라. 그리하면 <span>모든 지각</span>에 뛰어난 <span>하나님</span>의 <span>평강</span>이 <span>그리스도 예수 안</span>에서 <span>너희 마음</span>과 <span>생각</span>을 지키시리라',
         con2:'<span>너</span>는 <span>기도</span>할 때에 <span>네 골방</span>에 들어가 <span>문</span>을 닫고 <span>은밀한 중</span>에 계신 네 <span>아버지</span>께 <span>기도</span>하라 <span>은밀한 중</span>에 보시는 네 <span>아버지</span>께서 <span>갚으시</span>리라'
     },
     {
