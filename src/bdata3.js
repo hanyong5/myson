6522d1bb6bb456bc631d794dbeab3403e6888914
hanyong5 <hanyong5@naver.com>
2021-11-01 17:09:10 +0900

    Create bdata3.js

---
+++ src/bdata3.js
@@ -0,0 +1,90 @@
+export default [
+    {
+        idx:0,list:'1과' ,title:'성경의 권위',
+        line1:'베드로후서 1:21',line2:'여호수아 1:8',
+        con1:'<span>예언</span>은 <span>언제</span>든지 <span>사람</span>의 <span>뜻</span>으로 <span>낸것</span>이 아니오 오직 <span>성령</span>으로 <span>감동하심</span>을 받은 <span>사람</span>들이 <span>하나님</span>께 <span>받아 말한 것임</span>이라 ',
+        con2:'이 <span>율법책</span>을 네 <span>입</span>에서 <span>떠나지 말</span>게 하며 <span>주야</span>로 <span>그것</span>을 <span>묵상</span>하여 <span>그 안</span>에 <span>기록</span>된 대로 <span>다 지켜 행</span>하라 그리하면 네 <span>길</span>이 <span>평탄</span>하게 <span>될것</span>이며 네가 <span>형통</span>하리라. '
+    },
+    {
+        idx:1,list:'2과',title:'하나님은 누구신가?',
+        line1:'로마서 11:36상',line2:'예레미야 31:3하',
+        con1:'<span>이</span>는 <span>만물</span>이 <span>주에게</span>서 나오고 <span>주</span>로 <span>말미암</span>고 <span>주에게</span>로 <span>돌아감</span>이라',
+        con2:'<span>내가</span> 영원한 <span>사랑</span>으로 <span>너</span>를 <span>사랑하기</span>에 <span>인자함</span>으로 <span>너</span>를 <span>이끌</span>었다 <span>하였</span>노라'
+    },
+    {
+        idx:2,list:'3과',title:'예수 그리스도는 누구신가?',
+        line1:'히브리서 4:15',line2:'요한복음 14:6',
+        con1:'<span>우리</span>에게 있는 <span>대제사장</span>은 우리의 <span>연약함</span>을 <span>동정</span>하지 <span>못하실</span> 이가 아니요 <span>모든 일</span>에 <span>우리</span>와 똑같이 <span>시험</span>을 받으신 이로되 <span>죄</span>는 <span>없으</span>시니라',
+        con2:'<span>예수</span>께서 이르시되 내가 곧 <span>길</span>이요 <span>진리</span>요 <span>생명</span>이니 나로 말미암지 않고는 <span>아버지</span>께로 올자가 <span>없느</span>니라'
+    },
+    {
+        idx:3,list:'4과',title:'삼위일체 하나님',
+        line1:'요한복음 1:1',line2:'고린도후서 13:13',
+        con1:'<span>태초</span>에 <span>말씀</span>이 계시니라 이 <span>말씀</span>이 <span>하나님</span>과 함께 <span>계셨</span>으니 이 <span>말씀</span>은 곧 <span>하나님</span>이시니라',
+        con2:'주 예수 <span>그리스도</span>의 <span>은혜</span>와 <span>하나님</span>의 <span>사랑</span>과 성령의 <span>교통하심</span>이 너희 <span>무리</span>와 함께 <span>있을지</span>어다'
+    },
+    {
+        idx:4,list:'5과',title:'인간의 타락과 그결과',
+        line1:'로마서 5:12',line2:'히브리서 9:27',
+        con1:'그러므로 <span>한 사람</span>으로 말미암아 <span>죄</span>가 <span>세상</span>에 들어오고 <span>죄</span>로 말미암아 <span>사망</span>이 들어왔나니 이와 같이 <span>모든 사람</span>이 <span>죄</span>를 지었으므로 <span>사망</span>이 <span>모든 사람</span>에게 이르렀느니라',
+        con2:'한번 <span>죽는 것</span>은 <span>사람</span>에게 <span>정해진 것</span>이요. <span>그후</span>에는 <span>심판</span>이 있으리니'
+    },
+    {
+        idx:5,list:'6과',title:'예수 그리스도의 죽음',
+        line1:'로마서 5:8',line2:'갈라디아서 3:13',
+        con1:'<span>우리</span>가 <span>아직 죄인</span> 되었을 때에 <span>그리스도</span>께서 <span>우리</span>를 위하여 <span>죽으심</span>으로 <span>하나님</span>께서 <span>우리</span>에게 대한 <span>자기</span>의 사랑을 <span>확증</span>하셨느니라',
+        con2:'<span>그리스도</span>께서 <span>우리</span>를 위하여 <span>저주</span>를 받은바 되사 <span>율법</span>의 저주에서 <span>우리</span>를 <span>속량</span>하셨으니 기록된바 <span>나무</span>에 달린 <span>자마다 </span>저주 아래 있는 <span>자라</span> 하였음이라 '
+    },
+    {
+        idx:6,list:'7과',title:'예수그리스도의 부활',
+        line1:'로마서 4:25',line2:'갈라디아서 2:20',
+        con1:'<span>예수</span>는 우리 <span>범죄함</span>을 위하여 <span>내어줌</span>이 되고 또한 <span>우리</span>를 <span>의롭다 하심</span>을 위하여 <span>살아나셨느</span>니라',
+        con2:'내가 <span>그리스도</span>와 함께 <span>십자가</span>에 못 박혔나니 그런즉 <span>이제</span>는 내가 산 것이 아니요 <span>오직 내 안</span>에 <span>그리스도</span>께서 사신 것이라 <span>이제 내</span>가 <span>육체 가운데 사는 것</span>은 <span>나</span>를 <span>사랑</span>하사 <span>나</span>를 위하여 <span>자기 몸</span>을 버리신 <span>하나님</span>의 아들을 <span>믿는 믿음 안</span>에서 <span>사는 </span>것이라'
+    },
+    {
+        idx:7,list:'8과',title:'약속대로 오신 성령',
+        line1:'사도행전 2:38',line2:'고린도전서 12:13',
+        con1:'<span>베드로</span>가 가로되 <span>너희</span>가 <span>회개</span>하여 각각 <span>예수 그리스도</span>의 이름으로 <span>세례</span>를 받고 <span>죄 사함</span>을 얻으라 그리하면 <span>성령</span>을 <span>선물</span>로 받으리니',
+        con2:'<span>우리</span>가 <span>유대인</span>이나 <span>헬라인</span>이나 <span>종</span>이나 <span>자유자</span>나 다 <span>한 성령</span>으로 <span>세례</span>를 받아 <span>한 몸</span>이 되었고 또 다 <span>한 성령</span>을 마시게 하셨느니라 '
+    },
+    {
+        idx:8,list:'9과',title:'거듭난 사람',
+        line1:'디도서 3:5',line2:'데살로니가전서 1:3~4',
+        con1:'<span>우리</span>를 구원하시되 <span>우리</span>의 행한바 <span>의로운 행위</span>로 말미암지 아니하고 <span>오직</span> 그의 <span>긍휼하심</span>을 따라 <span>중생</span>의 <span>씻음</span>과 <span>성령</span>의 <span>새롭게 하심</span>으로 하셨나니',
+        con2:'<span>너희</span>의 <span>믿음</span>의 <span>역사</span>와 <span>사랑</span>의 수고와 우리 <span>주 예수 그리스도</span>에 대한 <span>소망</span>의 <span>인내</span>를 우리 <span>하나님 아버지 앞</span>에서  끊임없이 <span>기억함</span>이니 <span>하나님</span>의 사랑하심을 받은 <span>형제들</span>아 너희를 <span>택하심</span>을 아노라'
+    },
+    {
+        idx:9,list:'10과',title:'믿음이란 무엇인가?',
+        line1:'에베소서 2:8~9',line2:'로마서 4:18',
+        con1:'<span>너희</span>는 그 <span>은혜</span>에 의하여 <span>믿음</span>으로 말미암아 <span>구원</span>을 받았으니 이것은 <span>너희</span>에게 난 것이 아니요 <span>하나님</span>의 선물이라. <span>행위</span>에서 난것이 아니니 이는 <span>누구든지 자랑</span>하지 못하게 함이라',
+        con2:'<span>아브라함</span>이 <span>바랄 수 없</span>는 중에 <span>바라고 믿었</span>으니 이는 네 <span>후손</span>이 <span>이같으</span>리라 하신 <span>말씀대</span>로 많은 <span>민족</span>의 <span>조상</span>이 되게 하려 <span>하심</span>이라'
+    },
+    {
+        idx:10,list:'11과',title:'의롭다 함을 받은 은혜',
+        line1:'로마서 3장 21~22',line2:'로마서 8:32',
+        con1:'이제는 율법 외에 하나님의 한 의가 나타났으니 율법과 선지자들에게 증거를 받은 것이라 곧 예수 그리스도를 믿음으로 말미암아 모든 믿는 자에게 미치는 하나님의 의니 차별이 없느니라',
+        con2:'자기 아들을 아끼지 아니하시고 우리 모든 사람을 위하여 내주신 이가 어찌 그 아들과 함께 모든 것을 우리에게 주시지 아니하겠느냐'
+    },
+    {
+        idx:11,list:'12과',title:'우리안에 계시는 성령',
+        line1:'로마서 8장 26',line2:'갈라디아서 5:22~23',
+        con1:'이와 같이 성령도 우리의 연약함을 도우시나니 우리는 마땅히 기도할 바를 알지 못하나 오직 성령이 말할 수 없는 탄식으로 우리를 위하여 친히 간구하시느니라',
+        con2:'오직 성령의 열매는 사랑과 희락과 화평과 오래 참음과 자비와 양선과 충성과 온유와 절제니 이같은 것을 금지할 법이 없느니라'
+    },
+    {
+        idx:12,list:'13과',title:'그리스도인의 성화',
+        line1:'고린도후서 7:1',line2:'요한 1서 3:3',
+        con1:'그런즉 사랑하는 자들아 이 약속을 가진 우리는 하나님을 두려워하는 가운데서 거룩함을 온전히 이루어 육과 영의 온갖 더러운 것에서 자신을 깨끗하게 하자',
+        con2:'주를 향하여 이 소망을 가진 자마다 그의 깨끗하심과 같이 자기를 깨끗하게 하느니라'
+    },
+    {
+        idx:13,list:'14과',title:'예수 그리스도의 재림',
+        line1:'요한계시록 22:7',line2:'데살로니가전서 4:16',
+        con1:'보라 내가 속히 오리니 이 두루마리의 예언의 말씀을 지키는 자는 복이 있으리라 하더라',
+        con2:'주께서 호령과 천사장의 소리와 하나님의 나팔 소리로 친히 하늘로부터 강림하시리니 그리스도 안에서 죽은 자들이 먼저 일어나고'
+    }
+    
+    
+
+    
+]
