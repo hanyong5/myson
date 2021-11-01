03ce48c24f8e2e94cbbeeac93b0cd5dd0c70ae7c
hanyong5 <hanyong5@naver.com>
2021-11-01 17:08:33 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/home.vue'
 import blist1 from '../components/blist1.vue'
 import blist2 from '../components/blist2.vue'
+import blist3 from '../components/blist3.vue'
 import detail1 from '../components/detail1.vue'
 import detail2 from '../components/detail2.vue'
+import detail3 from '../components/detail3.vue'
 
 const routes = [
   {
@@ -33,6 +35,19 @@ const routes = [
     path: '/detail1/:id',
     name: 'detail1',
     component: detail1
+    
+  },
+  ,
+  {
+    path: '/list3',
+    name: 'list3',
+    component: blist3,
+    
+  },
+  {
+    path: '/detail3/:id',
+    name: 'detail3',
+    component: detail3
     
   }
 ]
