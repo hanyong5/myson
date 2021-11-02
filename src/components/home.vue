4dc906c618d5aa523a4763b712a41b58b2d60e34
hanyong5 <hanyong5@naver.com>
2021-11-02 17:15:22 +0900

    Update home.vue

--- src/components/home.vue
+++ src/components/home.vue
@@ -3,7 +3,7 @@
     <a href="http://m.cgntv.net/tv/31000/1.cgn" target="_blank">온라인 생명의 삶</a>
   </div>
   <div class="linkView">
-    <a href="intent://addshortcut?url="https://win-myson.netlify.app"&amp;icon="https://win-myson.netlify.app/img/icons/android-chrome-512x512.png"&amp;title="제자반암송"&oq=%ED%8C%A8%EC%85%98%ED%92%80&amp;serviceCode=sports&amp;version=7#Intent;scheme=naversearchapp;action=android.intent.action.VIEW;category=android.intent.category.BROWSABLE;package=com.nhn.android.search;end" onclick="nclk(this,'fot.shorcut','','',event);" class="u_sc">
+    <a href="intent://addshortcut?url=https://win-myson.netlify.app&amp;icon=https://win-myson.netlify.app/img/icons/android-chrome-512x512.png&amp;title=예림교회&oq=%ED%8C%A8%EC%85%98%ED%92%80&amp;serviceCode=sports&amp;version=7#Intent;scheme=naversearchapp;action=android.intent.action.VIEW;category=android.intent.category.BROWSABLE;package=com.nhn.android.search;end" onclick="nclk(this,'fot.shorcut','','',event);" class="u_sc">
       바로가기등록(안드로이드만요)
     </a>
   </div>
